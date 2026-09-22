9350fc96efe961299bcefd8f0644db4b54f3f8d4
106510262 <you@example.com>
2026-09-22 13:53:19 +1000

    Created file 'daysarray.php'

---
+++ daysarray.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Grading System">
+    <meta name="keywords" content="Heading 1, Title, Body, PHP">
+    <meta name="author" content="Olisaemeka Madu">
+    <title>Using PHP variables, arrays and operators - Task 2</title>
+</head>
+
+<body>
+
+    <h1>PHP variables, arrays and operators</h1>
+
+<?php
+        $days = array ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
+        echo "<p>The days of the week in English are: </p>";
+        echo $days[];
+?>
+
+</body>
+</html>
--- myfirst.php
+++ myfirst.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang ="en">
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8">
+    <meta name="description" content="Grading System">
+    <meta name="keywords" content="Heading 1, Title, Body, PHP">
+    <meta name="author" content="Olisaemeka Madu">
     <title>Using PHP variables, arrays and operators</title>
 </head>
 
-<body?>
+<body>
+
     <h1>PHP variables, arrays and operators</h1>
-    <?php
-    $marks = array (85, 85, 95);
-    $marks[1] = 90;
-    $ave = ($marks[0] + $marks[1] + $marks[2])/3;
 
-    if ($ave >= 50)
-        $status = "PASSED";
-    else
-        $status = "FAILED";
+<?php
+        $marks = array (85, 85, 95);
+        $marks[1] = 90;
+        $ave = ($marks[0] + $marks[1] + $marks[2])/3;
 
-    echo "<p>The average score is $ave. You $status.</p>";
-    ?>
+        if ($ave >= 50)
+            $status = "PASSED";
+        else
+            $status = "FAILED";
+
+        echo "<p>The average score is $ave. You $status.</p>";
+?>
+
 </body>
 </html>
